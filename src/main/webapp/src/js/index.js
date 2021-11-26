c01e70ce3a69c8be8147a8b13a23ce92a9213049
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-26 13:39:28 +0200

    Create basic simple user interface

--- src/main/webapp/src/js/index.js
+++ src/main/webapp/src/js/index.js
@@ -1,1 +1,17 @@
-console.log("success!!!!!");
+import Stepper from 'bs-stepper';
+
+window.addEventListener("load", function() {
+    let previousStepBtn = document.querySelector('#previous-step-btn');
+    let nextStepBtn = document.querySelector('#next-step-btn');
+    let stepper = new Stepper(document.querySelector('.bs-stepper'));;
+
+
+    previousStepBtn.addEventListener('click', function () {
+        stepper.previous();
+    });
+
+    nextStepBtn[0].addEventListener('click', function () {
+        stepper.next();
+    });
+
+});
