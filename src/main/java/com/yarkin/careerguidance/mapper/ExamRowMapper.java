cad322e1994d1acf53c334e75cbbdbdafa768576
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-12-06 11:07:31 +0200

    Added html escape when print on screen

--- src/main/java/com/yarkin/careerguidance/mapper/ExamRowMapper.java
+++ src/main/java/com/yarkin/careerguidance/mapper/ExamRowMapper.java
@@ -4,12 +4,13 @@ import com.yarkin.careerguidance.entities.Exam;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.apache.commons.text.StringEscapeUtils;
 
 public class ExamRowMapper {
     public Exam mapRow(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("id");
-        String title = resultSet.getString("title");
-        String description = resultSet.getString("description");;
+        String title = StringEscapeUtils.escapeHtml4(resultSet.getString("title"));
+        String description = StringEscapeUtils.escapeHtml4(resultSet.getString("description"));
         Exam exam = Exam.builder()
                 .id(id)
                 .title(title)
